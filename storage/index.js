0433c2a210ae414ef3878f60a2c35db51ef09c61
Limkon <limkon@skvip.eu.org>
2025-08-02 16:49:30 +0800

    Update index.js

--- storage/index.js
+++ storage/index.js
@@ -1,13 +1,15 @@
 const webdavStorage = require('./webdav');
 
-// 移除 config.json 的读写，因为我们将从数据库管理设定
-
-// getStorage 函数现在需要知道是哪个 WebDAV 挂载点
+/**
+ * 获取储存处理模块。
+ * 在当前设计中，所有操作都通过 webdavStorage 处理。
+ * @param {number} [mountId] - WebDAV 挂载点的 ID，用于确定使用哪个 WebDAV 实例。
+ * @returns {object} 储存处理模块。
+ */
 function getStorage(mountId) {
-    // 每次都返回 webdavStorage 模块，具体客户端实例在其内部处理
+    // 总是返回 webdavStorage 模块，具体的客户端实例在其内部根据 mountId 进行管理。
     return webdavStorage;
 }
-
 
 module.exports = {
     getStorage,
